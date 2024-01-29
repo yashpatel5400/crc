23757662fa7d62b4f890b459771a00d338c061bc
yashpatel5400 <ypatel5400@gmail.com>
2024-01-29 09:29:58 -0500

    Check in policy gradient base library

---
+++ polgrad_multinoise/pickle_io.py
@@ -0,0 +1,16 @@
+import pickle
+from utility import create_directory
+
+
+def pickle_import(filename_in):
+    pickle_off = open(filename_in,"rb")
+    data = pickle.load(pickle_off)
+    pickle_off.close()
+    return data
+
+
+def pickle_export(dirname_out, filename_out, data):
+    create_directory(dirname_out)
+    pickle_on = open(filename_out,"wb")
+    pickle.dump(data, pickle_on)
+    pickle_on.close()
