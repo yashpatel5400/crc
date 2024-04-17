a889b365b3aea97d9f17d1730cffd228c728bf9a
yashpatel5400 <ypatel5400@gmail.com>
2024-04-17 14:42:39 -0400

    Add airflow example

--- policygradient.py
+++ policygradient.py
@@ -788,7 +788,7 @@ def proj(C, C_hat, norm, q_hat, K):
     C_proj = cp.Variable(C.shape)
     constraints = [
         cp.atoms.norm(C_hat - C_proj, norm) <= q_hat, # keep proj in B_q(C_hat) with the ball defined in matrix 2-norm
-        cp.atoms.norm(C_proj @ W) <= 1 # ensures dynamics remain stabilizing
+        # cp.atoms.norm(C_proj @ W) <= 1 # ensures dynamics remain stabilizing
     ]
     prob = cp.Problem(cp.Minimize(cp.atoms.norm(C - C_proj)),
                     constraints)
