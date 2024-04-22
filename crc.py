f228e24e3d41af15380c8d660f37dce12dea3062
yashpatel5400 <ypatel5400@gmail.com>
2024-04-22 14:14:16 -0400

    Add robust alternatives to compare against

--- crc.py
+++ crc.py
@@ -9,9 +9,116 @@ import multiprocessing
 import torch
 
 from policygradient import PolicyGradientOptions, run_policy_gradient, run_dynamics_gradient, Regularizer
-from ltimult import LQRSysMult
+from ltimult import LQRSysMult, dare_mult, dlyap_mult
 
-def init_system(C, K_0):
+def get_noise(mult_noise_method, noise, nom_system_params):
+    A, B, Q, R, S0 = nom_system_params
+    n, m = B.shape
+
+    # Multiplicative noise data
+    p = 2  # Number of multiplicative noises on A
+    q = 2  # Number of multiplicative noises on B
+
+    if mult_noise_method == 'random':
+        Aa = np.random.standard_normal([n, n, p])
+        Bb = np.random.standard_normal([n, m, q])
+    elif mult_noise_method == 'rowcol':
+        # Pick a random row and column
+        Aa = np.zeros([n, n, p])
+        Bb = np.zeros([n, m, q])
+
+        Aa[np.random.randint(n), :, 0] = np.ones(n)
+        Aa[:, np.random.randint(n), 1] = np.ones(n)
+
+        Bb[np.random.randint(n), :, 0] = np.ones(m)
+        Bb[:, np.random.randint(m), 1] = np.ones(n)
+    elif mult_noise_method == 'random_plus_rowcol':
+        Aa = 0.3 * np.random.standard_normal([n, n, p])
+        Bb = 0.3 * np.random.standard_normal([n, m, q])
+        # Pick a random row and column
+        Aa[np.random.randint(n), :, 0] = np.ones(n)
+        Aa[:, np.random.randint(n), 1] = np.ones(n)
+        Bb[np.random.randint(n), :, 0] = np.ones(m)
+        Bb[:, np.random.randint(m), 1] = np.ones(n)
+
+    incval = 1.05
+    decval = 1.00 * (1 / incval)
+    weakval = 0.90
+
+    # a = np.random.standard_normal([p,1])
+    # b = np.random.standard_normal([q,1])
+    a = np.ones([p, 1])
+    b = np.ones([q, 1])
+    a = a * (float(1) / (p * n**2))  # scale as rough heuristic
+    b = b * (float(1) / (q * m**2))  # scale as rough heuristic
+
+    #    noise = 'weak'
+    if noise == 'weak' or noise == 'critical':
+        # Ensure near-critically mean square stabilizable
+        # increase noise if not
+        P, Kare = dare_mult(A, B, a, Aa, b, Bb, Q, R, show_warn=False)
+        mss = True
+        while mss:
+            if Kare is None:
+                mss = False
+            else:
+                a = incval * a
+                b = incval * b
+                P, Kare = dare_mult(A, B, a, Aa, b, Bb, Q, R, show_warn=False)
+        # Extra mean square stabilizability margin
+        a = decval * a
+        b = decval * b
+        if noise == 'weak':
+            #            print('Multiplicative noise set weak')
+            a = weakval * a
+            b = weakval * b
+    elif noise == 'olmss_weak' or noise == 'olmss_critical':
+        # Ensure near-critically open-loop mean-square stable
+        # increase noise if not
+        K0 = np.zeros([m, n])
+        P = dlyap_mult(A, B, K0, a, Aa, b, Bb, Q, R, S0, matrixtype='P')
+        mss = True
+        while mss:
+            if P is None:
+                mss = False
+            else:
+                a = incval * a
+                b = incval * b
+                P = dlyap_mult(A, B, K0, a, Aa, b, Bb, Q, R, S0, matrixtype='P')
+        # Extra mean square stabilizability margin
+        a = decval * a
+        b = decval * b
+        if noise == 'olmss_weak':
+            #            print('Multiplicative noise set to open-loop mean-square stable')
+            a = weakval * a
+            b = weakval * b
+    elif noise == 'olmsus':
+        # Ensure near-critically open-loop mean-square unstable
+        # increase noise if not
+        K0 = np.zeros([m, n])
+        P = dlyap_mult(A, B, K0, a, Aa, b, Bb, Q, R, S0, matrixtype='P')
+        mss = True
+        while mss:
+            if P is None:
+                mss = False
+            else:
+                a = incval * a
+                b = incval * b
+                P = dlyap_mult(A, B, K0, a, Aa, b, Bb, Q, R, S0, matrixtype='P')
+    #        # Extra mean square stabilizability margin
+    #        a = decval*a
+    #        b = decval*b
+    #        print('Multiplicative noise set to open-loop mean-square unstable')
+    elif noise == 'none':
+        print('MULTIPLICATIVE NOISE SET TO ZERO!!!')
+        a = np.zeros([p, 1])  # For testing only - no noise
+        b = np.zeros([q, 1])  # For testing only - no noise
+    else:
+        raise Exception('Invalid noise setting chosen')
+
+    return a, Aa, b, Bb
+
+def init_system(mult_noise_method, noise, C, K_0):
     A, B = C[:,:4], C[:,4:]
     
     # System problem data
@@ -20,13 +127,15 @@ def init_system(C, K_0):
     S0 = np.eye(A.shape[-1])
 
     # useless for us, but just keep to use library code
-    Aa = np.zeros(A.shape)
-    Aa = Aa[:, :, np.newaxis]
-    Bb = np.zeros(B.shape)
-    Bb = Bb[:, :, np.newaxis]
-    a = np.zeros((1, 1))
-    b = np.zeros((1, 1))
-
+    if noise == "none":
+        Aa = np.zeros(A.shape)
+        Aa = Aa[:, :, np.newaxis]
+        Bb = np.zeros(B.shape)
+        Bb = Bb[:, :, np.newaxis]
+        a = np.zeros((1, 1))
+        b = np.zeros((1, 1))
+    else:
+        a, Aa, b, Bb = get_noise(mult_noise_method, noise, (A, B, Q, R, S0))
     SS = LQRSysMult(A, B, a, Aa, b, Bb, Q, R, S0)
     SS.setK(K_0)
     return SS
@@ -52,39 +161,51 @@ def get_optimizer(K_size, K_steps):
                                     display_inplace=True,
                                     slow=False)
 
-def crc(args):
-    C, C_hat, q_hat = args
-       
-    # Get nominal system solution (should be bounded above by robust optimal value unless there's a bug)
-    # To find K^*, we can use PG but can get exact soln w/ Riccati Equations
+def eval_controller(C, K):
+    nominal_system = init_system("random", "none", C, K)
+    return nominal_system.c
+
+def crc(C, C_hat, q_hat):
     A, B = C[:,:4], C[:,4:]
     K_shape = ([B.shape[1], A.shape[1]])
-    nominal_system = init_system(C, np.zeros(K_shape))
-    nominal_system.setK(nominal_system.Kare)
-    nominal_cost = nominal_system.c
 
     try:
         # Solve robust system using policy gradient (based on Danskin's Theorem)
-        robust_system_init = init_system(C_hat, np.zeros(K_shape))
-        K_star     = robust_system_init.Kare # np.zeros(K_shape) # get 
+        robust_system_init = init_system("random", "none", C_hat, np.zeros(K_shape))
+        K_star     = robust_system_init.Kare # initial with nominal ARE solution 
         robust_pgo = get_optimizer(np.prod(K_shape), 1) # optimization for K is done in single steps to give correct gradients
         opt_steps  = 200
         for opt_step in range(opt_steps):
             print(f"Step: {opt_step}")
             
-            robust_system = init_system(C_hat, K_star) # (i.e. using predictions from generative model (A, B))
+            robust_system = init_system("random", "none", C_hat, K_star) # (i.e. using predictions from generative model (A, B))
             C_star, _ = run_dynamics_gradient(robust_system, robust_pgo, norm=2, q_hat=q_hat) # find the C^* = [A^*, B^*] for Danskin's Theorem (with fixed controller K^(t))
             
             print("---------------------------------------")
-            robust_system_star  = init_system(C_star, K_star) # fix C^* from above and take a step to update K
-            K_star, robust_cost = run_policy_gradient(robust_system_star, robust_pgo)
-        
-            # Print the regularized optimal gains (from proximal gradient optimization)
-            # and the unregularized optimal gains (from solving a Riccati equation)
-            print(f"Nominal Cost : {nominal_cost} | Robust Cost : {robust_cost}")
+            robust_system_star  = init_system("random", "none", C_star, K_star) # fix C^* from above and take a step to update K
+            K_star, _ = run_policy_gradient(robust_system_star, robust_pgo)
+        return K_star
     except:
-        robust_cost = np.inf # if prediction region is too large, GDA falls outside stabilizing region
-    return nominal_cost, robust_cost
+        return None # if prediction region is too large, GDA falls outside stabilizing region
+    
+def get_ctrls(args):
+    C, C_hat, q_hat = args
+    A, B = C[:,:4], C[:,4:]
+    K_shape = ([B.shape[1], A.shape[1]])
+    PGO = get_optimizer(np.prod(K_shape), K_steps=1_000)
+
+    ctrls = {}
+    ctrls["optimal"] = init_system("random", "none", C, np.zeros(K_shape)).Kare
+    ctrls["nominal"] = init_system("random", "none", C_hat, np.zeros(K_shape)).Kare
+    for mult_noise_method in ["random", "rowcol"]:
+        for noise in ["critical", "olmss_weak", "olmsus"]:
+            SS = init_system(mult_noise_method, noise, C, ctrls["nominal"])
+            try:
+                ctrls[f"{mult_noise_method}-{noise}"] = run_policy_gradient(SS, PGO)
+            except:
+                ctrls[f"{mult_noise_method}-{noise}"] = None
+    ctrls["crc"] = crc(C, C_hat, q_hat)
+    return ctrls
 
 if __name__ == "__main__":
     with open("experiments/airfoil.pkl", "rb") as f:
@@ -92,10 +213,9 @@ if __name__ == "__main__":
 
     workers = 20
     pool = multiprocessing.Pool(workers)
-    crc_results = np.array(list(zip(*pool.map(
-       crc,
+    controllers = list(pool.map(
+       get_ctrls,
        [(cfg["test_C"][C_idx], cfg["test_C_hat"][C_idx], cfg["q_hat"]) for C_idx in range(len(cfg["test_C"]))]
-       )
-    ))).T
-    results = pd.DataFrame({'nominal': crc_results[:,0], 'robust': crc_results[:,1]})
-    results.to_csv("results.csv")
+    ))
+    with open("controllers.pkl", "wb") as f:
+        pickle.dump(controllers, f)
